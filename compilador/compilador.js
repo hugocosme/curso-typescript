0b8ab8695f99fcf495fbf09f5e54a9d4fdeb3ca9
Hugo Cosme <hugo@carenet.com.br>
2022-03-14 16:08:29 -0300

    learning noEmitOnError

---
+++ compilador/compilador.js
@@ -0,0 +1,6 @@
+"use strict";
+var canal = 'Gaveta';
+var inscritos = 234245345;
+//canal = inscritos
+console.log("Canal = ".concat(canal));
+//let nome = 'Pedro' //não posso atribuir uma variavel com outro valor, quando ela já foi declarada noutro arquivo.
---
+++ compilador/compilador.ts
@@ -0,0 +1,7 @@
+let canal: string = 'Gaveta'
+let inscritos: number = 234245345
+
+//canal = inscritos
+console.log(`Canal = ${canal}`)
+
+//let nome = 'Pedro' //não posso atribuir uma variavel com outro valor, quando ela já foi declarada noutro arquivo.
