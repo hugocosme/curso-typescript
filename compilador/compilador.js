38d0748dca1d085e395628abe66d8b51020cfeb7
Hugo Cosme <hugo@carenet.com.br>
2022-03-14 16:33:54 -0300

    learning noImplicitAny https://www.typescriptlang.org/tsconfig\#noImplicitAny

--- compilador/compilador.js
+++ compilador/compilador.js
@@ -4,4 +4,10 @@ var inscritos = 234245345;
 //canal = inscritos
 console.log("Canal = ".concat(canal));
 //let nome = 'Pedro' //não posso atribuir uma variavel com outro valor, quando ela já foi declarada noutro arquivo.
+function soma(a, b) {
+    return a + b;
+}
+var qualquerCoisa;
+//qualquerCoisa = 12
+qualquerCoisa = 'ABC';
 //# sourceMappingURL=compilador.js.map
--- compilador/compilador.ts
+++ compilador/compilador.ts
@@ -5,3 +5,11 @@ let inscritos: number = 234245345
 console.log(`Canal = ${canal}`)
 
 //let nome = 'Pedro' //não posso atribuir uma variavel com outro valor, quando ela já foi declarada noutro arquivo.
+
+function soma(a: any, b: any) {
+    return a + b
+}
+
+let qualquerCoisa
+//qualquerCoisa = 12
+qualquerCoisa = 'ABC'
